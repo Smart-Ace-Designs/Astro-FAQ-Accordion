ae8122b0747a7412bda1a251ad1af92c4032e6dc
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2026-03-23 14:35:27 -0500

    Fix base URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://smart-ace-designs.github.io",
-  base: "/astro-qrcode-component",
+  base: "/astro-faq-accordion",
   trailingSlash: "always",
   vite: {
     plugins: [tailwindcss()],
